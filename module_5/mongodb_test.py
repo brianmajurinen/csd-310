03a27fe5ac890ec19482f0c94a2c192c0497e8d8
brianmajurinen <83433864+brianmajurinen@users.noreply.github.com>
2022-02-04 17:29:43 -0600

    Add files via upload

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,6 @@
-a
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.pmhbw.mongodb.net/pytech?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
