4c4630b4ba35f80f21f489f94b8da5bb8af01ea9
brianmajurinen <83433864+brianmajurinen@users.noreply.github.com>
2022-02-19 15:51:27 -0600

    Add files via upload

---
+++ module_8/mysql_test.py
@@ -0,0 +1,11 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports_user",
+    "password": "password",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
