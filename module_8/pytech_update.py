e7b7f6a530a152992052b0082080bf35b12e214e
brianmajurinen <83433864+brianmajurinen@users.noreply.github.com>
2022-02-23 14:54:55 -0600

    Add files via upload

---
+++ module_8/pytech_update.py
@@ -0,0 +1,50 @@
+#Brian Majurinen
+#Bellevue University
+#CYBR410-H331
+import mysql.connector
+from mysql.connector import errorcode
+#Issues adding a user, so went with root
+config = {
+    "user": "root",
+    "password": "!M!ysql123",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try: 
+    #connecting to pysports, this is mostly the code from the course github
+    db = mysql.connector.connect(**config)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT team_id, team_name, mascot FROM team")
+ 
+    teams = cursor.fetchall()
+
+    print("\n  -- DISPLAYING TEAM RECORDS --")
+    
+    # iterate over the teams data set and display the results 
+    for team in teams: 
+        print("  Team ID: {}\n  Team Name: {}\n  Mascot: {}\n".format(team[0], team[1], team[2]))
+
+    # select query for the player table 
+    cursor.execute("SELECT player_id, first_name, last_name, team_id FROM player")
+
+    # get the results from the cursor object 
+    players = cursor.fetchall()
+
+    print ("\n  -- DISPLAYING PLAYER RECORDS --")
+
+    # iterate over the players data set and display the results
+    for player in players:
+        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team ID: {}\n".format(player[0], player[1], player[2], player[3]))
+
+    input("\n\n  Press any key to continue... ")
+
+#Didn't use the error codes here
+
+finally:
+    """ close the connection to MySQL """
+    
+    db.close()
