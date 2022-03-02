1968418c3db6fece9667ec8f7fbb0ca4a1889d83
brianmajurinen <83433864+brianmajurinen@users.noreply.github.com>
2022-03-02 13:54:04 -0600

    Update db_init.sql

--- module_12/db_init.sql
+++ module_12/db_init.sql
@@ -1,20 +1,6 @@
 CREATE DATABASE whatabook;
 use whatabook;
 
-CREATE TABLE store (
-    store_id    INT             NOT NULL    AUTO_INCREMENT,
-    locale      VARCHAR(500)    NOT NULL,
-    PRIMARY KEY(store_id)
-);
-
-CREATE TABLE book (
-    book_id     INT             NOT NULL    AUTO_INCREMENT,
-    book_name   VARCHAR(200)    NOT NULL,
-    author      VARCHAR(200)    NOT NULL,
-    details     VARCHAR(500),
-    PRIMARY KEY(book_id)
-);
-
 CREATE TABLE user (
     user_id         INT         NOT NULL    AUTO_INCREMENT,
     first_name      VARCHAR(75) NOT NULL,
@@ -33,6 +19,20 @@ CREATE TABLE wishlist (
     CONSTRAINT fk_user
     FOREIGN KEY (user_id)
         REFERENCES user(user_Id)
+);
+
+CREATE TABLE book (
+    book_id     INT             NOT NULL    AUTO_INCREMENT,
+    book_name   VARCHAR(200)    NOT NULL,
+    author      VARCHAR(200)    NOT NULL,
+    details     VARCHAR(500),
+    PRIMARY KEY(book_id)
+);
+
+CREATE TABLE store (
+    store_id    INT             NOT NULL    AUTO_INCREMENT,
+    locale      VARCHAR(500)    NOT NULL,
+    PRIMARY KEY(store_id)
 );
 
 /*store*/
